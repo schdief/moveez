3d5495699d6f48f544d080454062088bea9131da
deradre <41394765+deradre@users.noreply.github.com>
2018-11-30 20:08:38 +0100

    add text and enter button to welcome page

--- src/app/views/public/landingPage/index.js
+++ src/app/views/public/landingPage/index.js
@@ -6,9 +6,38 @@ import sBackgroundImageUrl from "./welcome_background.png";
 
 const Background = ()=>{
     return (
-        <div style={{width: "100%", height:"500px", backgroundImage:`url(${sBackgroundImageUrl})`, backgroundSize:"cover"}}/>
+        <div style={{
+            width: "100%", 
+            height:"500px", 
+            backgroundImage:`url(${sBackgroundImageUrl})`, 
+            backgroundSize:"cover",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column"}}>
+            <BingeText/>
+            <SignupButton/>
+        </div>
     );
-}
+};
+
+const BingeText = ()=>{
+    return (
+        <p style={{
+            fontSize: "5rem",
+            color: "lightgrey"
+        }}>manage your binge</p>
+    );
+};
+
+const SignupButton = ()=>{
+    const fnOnClick = ()=>{
+        open("/title", "_self");
+    };
+    return (
+        <button className="ui button"onClick={fnOnClick}>Enter Moveez</button>
+    );
+};
 
 ReactDOM.render(
     <Background/>,
