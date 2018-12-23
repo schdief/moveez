9c0d411c13fcac0ad79a47eb2b6234d3b20ebfff
deradre <41394765+deradre@users.noreply.github.com>
2018-12-23 21:40:46 +0100

    improve layout of landingpage
    - make it fullscreen
    - properly integrate footer
    - add logo
    - update background screenshot

--- src/app/views/public/landingPage/index.js
+++ src/app/views/public/landingPage/index.js
@@ -2,23 +2,68 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import sBackgroundImageUrl from "./welcome_background.png";
+import sLogoUrl from "../logo.png";
 
 
-const Background = ()=>{
+const LandingPage = ()=>{
+    return (
+        <div style={{
+            fontFamily:"Lato",
+            width: "100%", 
+            height:"100%", 
+            backgroundImage:`url(${sBackgroundImageUrl})`, 
+            backgroundSize:"cover"}}>
+            <FlexFix/>
+        </div>
+    );
+};
+
+const Content = ()=>{
     return (
         <div style={{
             width: "100%", 
-            height:"500px", 
-            backgroundImage:`url(${sBackgroundImageUrl})`, 
-            backgroundSize:"cover",
+            height:"100%", 
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             flexDirection: "column"}}>
+            <MoveezIcon/>
             <BingeText/>
             <SignupButton/>
         </div>
     );
+};
+
+const FlexFix = ()=>{
+    return (
+        <div style={{
+            width: "100%", 
+            height:"100%", 
+            display: "flex",
+            flexDirection: "column"}}>
+            <div style={{flexGrow: 1}}>
+                <Content/>
+            </div>
+            <div>
+                <MoveezFooter/>
+            </div>
+        </div>
+    );
+};
+
+const MoveezFooter = () => {
+    return (
+        <div style={{color:"white"}}>
+            <div>Logo made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Thanks to <a href="https://www.emojione.com/">EmojiOne</a> for providing free emoji icons.</div>
+        </div>
+    );
+};
+
+const MoveezIcon = () => {
+    return (
+        <img src={sLogoUrl}/>
+    )
 };
 
 const BingeText = ()=>{
@@ -40,6 +85,6 @@ const SignupButton = ()=>{
 };
 
 ReactDOM.render(
-    <Background/>,
+    <LandingPage/>,
     document.getElementById('reactRoot')
 )
